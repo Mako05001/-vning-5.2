c6ac723cc3f6988b8b75fc00551478e1c96e65e1
Pingvu <nhlpool27@gmail.com>
2023-01-20 13:59:44 +0100

    Add project files.

---
+++ Övning 5.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hur många tal vill du skriva?");
+int Tot = int.Parse(Console.ReadLine());
+int[] T = new int[Tot];
+Console.WriteLine("Skriv " + Tot + " Tal");
+for (int i = 0; i < Tot; i++)   {
+    T[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < Tot; i++)   {
+    Console.Write($"Tal {i+1}:");
+    Console.Write(T[i]);
+    Console.WriteLine();
+}
